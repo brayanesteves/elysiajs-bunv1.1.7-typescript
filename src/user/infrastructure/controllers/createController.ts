ff224ea3191d6ffab067344d4f1a426b61192483
Brayan Esteves <brayan.esteves93@gmail.com>
2024-05-07 18:34:22 -0400

    Controller(createController) [07/05/2024]

---
+++ src/user/infrastructure/controllers/createController.ts
@@ -0,0 +1,23 @@
+import { CreateUser } from "../../application/create.js";
+
+export class CreateUserController {
+
+    constructor(private createUser:CreateUser) {}
+
+    async run({ body }:any) {
+        try {
+            const user = await this.createUser.run(body.email, body.password);
+            return {
+                status:201,
+                  data:user,
+            };
+        } catch(e) {
+            const error = e as Error;
+            return {
+                status:500,                
+                message:error.message,
+            };
+        }
+    }
+
+}
