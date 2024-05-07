fff6ae942d2669fc7cbd558141be499e19ac1cf0
Brayan Esteves <brayan.esteves93@gmail.com>
2024-05-07 18:24:42 -0400

    Repository(User) [07/05/2024]

---
+++ src/user/infrastructure/UserRepository.ts
@@ -0,0 +1,23 @@
+import      { PrismaClient } from "@prisma/client";
+import type { IUser }        from "../domain/IUser.js";
+import      { User }         from "../domain/User.js";
+
+export class UserRepository implements IUser {
+
+    private db: PrismaClient;
+
+    constructor() {
+        this.db = new PrismaClient();
+    }
+
+    async create(email: string, password: string): Promise<User> {
+        const user = await this.db.user.create({
+            data: {
+                email:email,
+                password:password,
+            }
+        });
+        return new User(user.id, user.email, user.password);
+    }
+
+}
