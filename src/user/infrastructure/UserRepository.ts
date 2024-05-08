808204dd24ebe4c3d32c8e790448bfee4ae19ed2
Brayan Esteves <brayan.esteves93@gmail.com>
2024-05-07 23:07:13 -0400

    New function in UserRepository.ts [07/05/2024]

--- src/user/infrastructure/UserRepository.ts
+++ src/user/infrastructure/UserRepository.ts
@@ -20,4 +20,18 @@ export class UserRepository implements IUser {
         return new User(user.id, user.email, user.password);
     }
 
+    async find(email:string) : Promise<User | null> {
+        const user = await this.db.user.findUnique({
+            where:{
+                email
+            },
+        });
+
+        if(!user) {
+            return null;
+        }
+
+        return new User(user.id, user.email, user.password);
+    }
+
 }
