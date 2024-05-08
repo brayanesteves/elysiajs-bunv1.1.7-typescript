e919a7f5a945131fd6101dd7649d4ca5592267a0
Brayan Esteves <brayan.esteves93@gmail.com>
2024-05-07 23:03:29 -0400

    User DTO [07/05/2024]

--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -1,9 +1,5 @@
-import { Elysia, t }            from 'elysia';
-import { createUserController } from '../server/dependencies';
+import { Elysia,}                from 'elysia';
+import { createUserController, } from '../server/dependencies';
+import { createUserDTO, }        from './domain/userDTO';
 
-export const userRouter = new Elysia({ prefix:'/users' }).post('/', () => createUserController.run.bind(createUserController), {
-    body: t.Object({
-        email   :t.String(),
-        password:t.String()
-    })
-}).get('/',() => "Get all users");
+export const userRouter = new Elysia({ prefix:'/users' }).post('/', () => createUserController.run.bind(createUserController), createUserDTO).get('/',() => "Get all users");
